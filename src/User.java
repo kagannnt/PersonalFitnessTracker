34e7cfcf9b869afe0237efe0cfc974415b900a52
kagannnt <tasbolattkagan@gmail.com>
2025-12-23 10:44:22 +0500

    Addignment

--- src/User.java
+++ src/User.java
@@ -1,27 +1,24 @@
-class User {
+public class User {
     private String name;
+    private int age;
     private double weight;
-    private WorkoutRoutine routine;
 
-    public User(String name, double weight, WorkoutRoutine routine) {
+    public User(String name, int age, double weight) {
         this.name = name;
+        this.age = age;
         this.weight = weight;
-        this.routine = routine;
     }
 
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
+
     public double getWeight() { return weight; }
     public void setWeight(double weight) { this.weight = weight; }
 
-    public WorkoutRoutine getRoutine() { return routine; }
-    public void setRoutine(WorkoutRoutine routine) { this.routine = routine; }
-
     public void displayInfo() {
-        System.out.println("User: " + name + ", Weight: " + weight + "kg");
-        if (routine != null) {
-            routine.printDetails();
-        }
+        System.out.println("User: " + name + ", Age: " + age + ", Weight: " + weight + "kg");
     }
 }
